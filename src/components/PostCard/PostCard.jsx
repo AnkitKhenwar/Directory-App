9b028506e05448363482215c5db0189fc28ae06e
AnkitKhenwar <85917490+AnkitKhenwar@users.noreply.github.com>
2023-11-26 11:30:17 +0530

    Add files via upload

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -2,20 +2,12 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-
+import "./PostCard.css";
 export default function PostCard({ title, content }) {
   return (
-    <Card
-      sx={{
-        minWidth: 275,
-        minHeight: 170,
-        backgroundColor: "#C0E87B",
-        cursor: "pointer",
-      }}
-    >
+    <Card sx={{backgroundColor:"#C0E87B"}}  className="card">
       <CardContent>
-        <Typography
-          sx={{ fontSize: 14, textAlign: "center" }}
+        <Typography className="typography"
           color="text.secondary"
           gutterBottom
         >
